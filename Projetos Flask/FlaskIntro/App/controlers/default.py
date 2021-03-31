91d8aecfa2a58ed6da39738d4a9918431606d1f5
Felipe <felipe.rinaldini@gmail.com>
2021-03-31 00:29:28 -0300

    inserção de templates com Bootstrap

--- Projetos Flask/FlaskIntro/App/controlers/default.py
+++ Projetos Flask/FlaskIntro/App/controlers/default.py
@@ -1,16 +1,27 @@
 # arquivo separado para o controler --> função de Rota
 
 #importa a var "app" de __init__.py
+from flask import render_template
 from App import app
+
 
 # decorator --> é uma caracteristica do python. Ele entra sempre antes de uma função.
 # está aplicando o método 'route' sobre a função 'index'. Ele vai definir um rota para a página.
-@app.route("/index")
-@app.route("/")
-def index(): #por padrão, foi colocado o nome da pagina de index
-    return('Hello world!')
+@app.route("/index/<user>")
+@app.route("/", defaults={"user":None})
+def index(user): #por padrão, foi colocado o nome da pagina de index
+    return render_template('index.html', user=user) # função recebe uma arquivo .html e o renderiza # junto com o html, envia as variavel que utilizar na pagina. No caso, o Usuário
+    
 
 
+
+
+
+
+
+
+
+'''
 @app.route("/test", defaults={'name': None}) #define como padrão None, assim não dá erro caso çao tenha nome do usuário
 @app.route("/test/<name>")
 def teste(name):
@@ -21,3 +32,4 @@ def teste(name):
         return("Olá, usuário!")
 
 #@app.route("/test/", methods=['GET'])
+'''
